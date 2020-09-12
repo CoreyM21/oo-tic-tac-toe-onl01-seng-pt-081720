68cf32cfdc8536e40ea587f4544a4d66240adced
Corey Martin <coreyjmartin2@gmail.com>
2020-09-12 17:15:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -104,9 +104,13 @@ class TicTacToe
   end
   
   def play 
-    until over? == true 
-     turn
-   end
-  end 
+  turn until over?
+    # Congratulate the winner
+    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
+    # Ask if they'd like to play again
+    puts "Would you like to play again? (Y or N)"
+    # If yes, then #play again
+    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
+  end
   
 end
